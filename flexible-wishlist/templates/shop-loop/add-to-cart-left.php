38e7a9723fd08eb4f46997c16d7426f57772cd05
Sean Murphy <sean@iamseanmurphy.com>
2024-10-08 01:59:52 +0000

    1.2.21

---
+++ flexible-wishlist/templates/shop-loop/add-to-cart-left.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @var \WC_Product $product           .
+ * @var bool        $status            .
+ * @var string      $icon_type         .
+ * @var string      $text_status_add   .
+ * @var string      $text_status_added .
+ *
+ * @package WPDesk\FlexibleWishlist
+ */
+
+?>
+<span data-fw-action="add-to-wishlist"
+	tabindex="0"
+	aria-label="<?php esc_attr_e('Add to wishlist', 'flexible-wishlist') ?>"
+	class="fw-button fw-button--before <?php echo esc_attr( ( $status ) ? 'fw-button--active' : '' ); ?>"
+	data-product-id="<?php echo esc_attr( $product->get_id() ); ?>">
+	<span class="fw-button-icon fw-button-icon--<?php echo esc_attr( $icon_type ); ?>"></span>
+</span>
---
+++ flexible-wishlist/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpdesk/flexible-wishlist',
+        'pretty_version' => '1.2.21',
+        'version' => '1.2.21.0',
+        'reference' => 'a24c46e24cc50ba6433d39ac6ec5d0352fcdcd9f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpdesk/flexible-wishlist' => array(
+            'pretty_version' => '1.2.21',
+            'version' => '1.2.21.0',
+            'reference' => 'a24c46e24cc50ba6433d39ac6ec5d0352fcdcd9f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpdesk/wp-wpdesk-helper-override' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'reference' => '77844fc71da7ebb7645f10f0fc15f5294fed8542',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpdesk/wp-wpdesk-helper-override',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
